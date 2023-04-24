468089045e75cb06bd6d88670e01fa4743cd112d
Manish Kumar <manishhansalmeh@gmail.com>
2023-04-24 19:15:31 +0530

    Update

--- common_lib/jwt_token.js
+++ common_lib/jwt_token.js
@@ -1,16 +1,19 @@
 const jwt = require("jsonwebtoken");
 const SECRET_KEY = "MY_secret_key";
 
+// generateToken
 const generateToken = (payload) => {
   let token = jwt.sign(JSON.stringify(payload), SECRET_KEY);
   return token;
 }
 
+// verifyToken
 const verifyToken = (token) => {
   let data = jwt.verify(token, SECRET_KEY);
   return data;
 }
 
+// tokenDecode
 const tokenDecode = (token) => {
   let data = jwt.decode(token);
   return data;
